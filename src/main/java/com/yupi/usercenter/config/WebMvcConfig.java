2c2e3732c835a8b92b7a09649e344c40f6bd6b0b
dylan <1205428733@qq.com>
2023-01-22 15:44:39 +0800

    first commit3

--- src/main/java/com/yupi/usercenter/config/WebMvcConfig.java
+++ src/main/java/com/yupi/usercenter/config/WebMvcConfig.java
@@ -1,7 +1,29 @@
-package com.yupi.usercenter.config;/**
+package com.yupi.usercenter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
  * @Classname WebMvcConfig
  * @Description TODO
  * @Date 2023/1/22 15:35
  * @Created by dylan
- */public class WebMvcConfig {
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                // 是否发送Cookie
+                .allowCredentials(true)
+                // 放行哪些原始域
+                .allowedOrigins("*")
+                // 放行哪些请求方式
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                // 放行哪些原始请求头部信息
+                .allowedHeaders("*")
+                // 暴露哪些头部信息
+                .exposedHeaders("*");
+    }
 }
